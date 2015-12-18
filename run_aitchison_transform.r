eb4a0dc1b6218846e4fe1372010ecad2ebd6af97
ruthgrace <ruthgracewong@gmail.com>
2015-12-18 11:38:40 -0500

    fixed aitchison transform

--- run_aitchison_transform.r
+++ run_aitchison_transform.r
@@ -3,8 +3,6 @@ options(error=recover)
 
 source("/Volumes/data/ruth/scripts/AitchisonTransform.r")
 
-subsys4 <- function(filename="get_annotated_counts_output.txt",rounded=TRUE, subjects = 20, firstsubjectindex = 2, lastsubjectindex = 21, 
-groupindex = 22,lengthindex=2,outputfolder="/Volumes/data/ruth/scripts/")
-
-write.table(subsys4, file="/Volumes/data/ruth/scripts/AitchisonTransform_output.txt",sep="\t",quote=FALSE)
+subsys4 <- aitchison.transform.reads(filename="get_annotated_counts_output.txt",rounded=TRUE, subjects = 20, firstsubjectindex = 2, lastsubjectindex = 21, 
+groupindex = 22,lengthindex=2,outputfolder="../scripts")
 
