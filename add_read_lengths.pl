8c857be41271536676a2ee3b028ba442996a5fff
ruthgrace <ruthgracewong@gmail.com>
2015-12-23 01:57:51 -0500

    write add read lengths perl script (need to debug)

--- add_read_lengths.pl
+++ add_read_lengths.pl
@@ -9,4 +9,41 @@ print $help if !$ARGV[2];exit if !$ARGV[2];
 
 my $counts = $ARGV[0];
 my $refseqs = $ARGV[1];
-my $outfile = $ARGV[2];
+my $out = $ARGV[2];
+
+# read in all refseq lengths
+my %seqlengths;
+my $id;
+my $seqlength = -1;
+open (REFSEQFILE, "< $refseqs") or die "Could not open $refseqs\n";
+while(defined (my $l = <REFSEQFILE>)) {
+	chomp ($l);
+	if ($l =~ /^>/) {
+		$id = $l;
+		# strip > character
+		$id =~ s/^.//;
+		# trim
+		$id =~ s/^\s+|\s+$//g;
+		if ($seqlength != -1) {
+			$seqlengths{$id} = $seqlength;
+		}
+		$seqlength = 0;
+	}
+	else {
+		$seqlength = $seqlength + length($l);
+	}
+}
+close REFSEQFILE;
+
+my @countlineitems;
+open (COUNTFILE, "< $counts") or die "Could not open $counts\n";
+open (OUTFILE, "> $out") or die "Could not open $out\n";
+while(defined (my $l = <COUNTFILE>)) {
+	chomp ($l);
+	@countlineitems = split(/\t/, $l, 2);
+	$id = $countlineitems[0];
+	$id =~ s/^\s+|\s+$//g;
+	print OUTFILE "$id\t$seqlengths{$id}\tcountlineitems[1]\n";
+}
+close COUNTFILE;
+close OUTFILE;
