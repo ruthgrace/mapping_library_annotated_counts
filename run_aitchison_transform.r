9841746a00d27b365ddbeb51f92280ded5f9b9ce
ruthgrace <ruthgracewong@gmail.com>
2015-12-23 23:59:55 -0500

    fixed add read lengths perl script

--- run_aitchison_transform.r
+++ run_aitchison_transform.r
@@ -5,13 +5,15 @@ source("/Volumes/data/ruth/scripts/AitchisonTransform.r")
 
 subsys4 <- aitchison.transform.reads(filename="annotated_counts_with_refseq_length.txt",rounded=TRUE, subjects = 20, firstsubjectindex = 3, lastsubjectindex = 22, groupindex = 23,lengthindex=2,outputfolder="subsys4_counts")
 
-d <- read.table("annotated_counts_with_refseq_length.txt",sep="\t",fill=TRUE,header=TRUE)
 
-d.subsys1 <- d[which(!is.na(d$subsys1)),]
-d.subsys2 <- d[which(!is.na(d$subsys2)),]
-d.subsys3 <- d[which(!is.na(d$subsys3)),]
+### output tables for other subsys (in my case actually only subsys2 has NA)
+# d <- read.table("annotated_counts_with_refseq_length.txt",sep="\t",fill=TRUE,header=TRUE,quote="",comment.char="")
 
-write.table(d.subsys1,file="get_annotated_counts_subsys1_output.txt",sep="\t",quote=FALSE)
-write.table(d.subsys2,file="get_annotated_counts_subsys2_output.txt",sep="\t",quote=FALSE)
-write.table(d.subsys3,file="get_annotated_counts_subsys3_output.txt",sep="\t",quote=FALSE)
+# d.subsys1 <- d[which(!is.na(d$subsys1)),]
+# d.subsys2 <- d[which(!is.na(d$subsys2)),]
+# d.subsys3 <- d[which(!is.na(d$subsys3)),]
+
+# write.table(d.subsys1,file="annotated_counts_subsys1.txt",sep="\t",quote=FALSE)
+# write.table(d.subsys2,file="annotated_counts_subsys2.txt",sep="\t",quote=FALSE)
+# write.table(d.subsys3,file="annotated_counts_subsys3.txt",sep="\t",quote=FALSE)
 
