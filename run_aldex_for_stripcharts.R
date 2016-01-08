badeb13deba07f4419e5b4ae1f26b34b935fdd14
ruthgrace <ruthgracewong@gmail.com>
2016-01-08 14:42:11 -0500

    updated filename in run_aldex for stripchats

--- run_aldex_for_stripcharts.R
+++ run_aldex_for_stripcharts.R
@@ -16,7 +16,7 @@ nash <- c("CL_166_BL_R2_R1", "CL_169_BL_R1", "CL_139_BL_2_R1", "CL_173_2_R1", "C
 healthy <- c("HLD_100_R1", "HLD_102_R1", "HLD_111_2_R1", "HLD_80_R1", "HLD_85_R1", "HLD_28_R1", "HLD_47_R1", "HLD_72_2_R1", "HLD_112_R1", "HLD_23_R1")
 
 # read in fully annotated counts with hierarchies
-d <- read.table("annotated_counts_with_refseq_length.txt",sep="\t",header=TRUE,row.names=NULL,quote="",comment.char="")
+d <- read.table("annotated_counts_with_refseq_length_non_zero_features.txt",sep="\t",header=TRUE,row.names=NULL,quote="",comment.char="")
 
 originaldata <- d
 
