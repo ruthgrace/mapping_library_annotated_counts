915e0a15fc4e0bd5c1461a83e79ebc7ec5ba770b
ruthgrace <ruthgracewong@gmail.com>
2016-01-12 17:29:08 -0500

    dbug

--- get_unmapped.sh
+++ get_unmapped.sh
@@ -2,15 +2,12 @@ dir=$1;
 echo "the input argument is $dir."
 for D in `find $dir -type d`
 do
-    echo "dir is $D"
     sample=$(basename $D);
-    echo "sample is $sample after basename";
     sample=${sample::-4};
-    echo "sample is $sample";
-    samfile=$sample".sam";
+    samfile=$D"/"$sample".sam";
     echo "samfile is $samfile";
-    unmapped=$sample"_unmapped.sam";
+    unmapped=$D"/"$sample"_unmapped.sam";
     echo "unmapped file is $unmapped";
-    unmappedFasta=$sample"_unmapped.fa";
+    unmappedFasta=$D"/"$sample"_unmapped.fa";
     echo "unmapped fasta is $unmappedFasta";
 done
